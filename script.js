80c4f9aedad1dc3327997afcd36f24b82dd84fa2
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2023-03-12 17:59:32 +0100

    minusTime()

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const strings = ["E", "A", "D", "G", "B", "e"];
 let enharmonicsOn = false;
 let isAutorun = false;
 let autorunIntervalID;
+let seconds = 5;
 
 // *** QUERYSELECTORS ***
 const display = document.querySelector(".main-display");
 const randomizer = document.querySelector(".randomizer");
 const toggleNotes = document.querySelector(".toggleNotes");
 const autorunButton = document.querySelector(".autorun");
+const minusButton = document.querySelector(".minus");
+const plusButton = document.querySelector(".plus");
 
 // const notes = ["Ab", "A", "A#", "Bb", "B", "Cb", "C", "C#", "Db", "D", "Eb", "E",
 //               "E#", "Fb", "F", "F#", "Gb", "G", "G#"];
@@ -39,24 +42,6 @@ function randomize() {
   display.textContent = randomNote + " on " + randomString + " string";
 }
 
-autorunButton.addEventListener("click", autorun);
-document.addEventListener("keypress", function(e) {
-  if(e.keyCode === 32) {
-    autorun();
-  }
-});
-
-function autorun() {
-  autorunButton.classList.toggle("autorun_active");
-  if(!isAutorun) {
-    randomize();
-    autorunIntervalID = setInterval(randomize, 5000);
-    isAutorun = true;
-  } else {
-    window.clearInterval(autorunIntervalID);
-    isAutorun = false
-  }
-}
 
 toggleNotes.addEventListener("click", toggleEnharmonics);
 document.addEventListener("keypress", e => {
@@ -71,5 +56,36 @@ function toggleEnharmonics() {
     enharmonicsOn = false;
   } else {
     enharmonicsOn = true;
+  }
+}
+
+autorunButton.addEventListener("click", autorun);
+document.addEventListener("keypress", function(e) {
+  if(e.keyCode === 32) {
+    autorun();
+  }
+  console.log(e.keyCode);
+});
+
+function autorun() {
+  autorunButton.classList.toggle("autorun_active");
+  if(!isAutorun) {
+    randomize();
+    autorunIntervalID = setInterval(randomize, 5000);
+    isAutorun = true;
+  } else {
+    window.clearInterval(autorunIntervalID);
+    isAutorun = false
+  }
+}
+
+minusButton.addEventListener("click", minusTime);
+
+function minusTime() {
+  if(seconds > 1) {
+    seconds -= 1
+    autorunButton.innerHTML = `auto ${seconds}s`;
+  } else {
+    return
   }
 }
